49722617024528cf3fc7aecbb8096de28ce377a4
andre <Andrews.ru83@gmail.com>
2023-04-06 16:52:39 +0300

    Commit

---
+++ src/main/java/pro/sky/attestation/AutomationOfStockAccounting/AutomationOfStockAccountingApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.attestation.AutomationOfStockAccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationOfStockAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationOfStockAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/attestation/AutomationOfStockAccounting/AutomationOfStockAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.attestation.AutomationOfStockAccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationOfStockAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
